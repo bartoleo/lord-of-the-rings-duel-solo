f43b7a328ec23325bd30cf0b72ef141a2c119ea2
Leo Bartoloni <bartololeo74@gmail.com>
2024-11-02 15:40:21 +0100

    [UPD] added PWA configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+import fg from 'fast-glob'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    VitePWA({
+      injectManifest: {
+        maximumFileSizeToCacheInBytes: 3 * 1024 ** 2,
+        globPatterns: ['**/*.{js,jsx,css,html,png,svg,woff2,ttf,ico,json,webp,mp3,wav}'],
+      }
+    })
+  ]
 })
+
